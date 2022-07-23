b9ed08fb691e63e101509e0db3bf2236ee311927
cozyzoey <devzoeykim@gmail.com>
2022-07-23 22:57:52 +0900

    feat: react-toastify 추가

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,24 @@
+import { ToastContainer } from 'react-toastify'
 import '../styles/globals.scss'
+import 'react-toastify/dist/ReactToastify.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  )
 }
 
 export default MyApp
