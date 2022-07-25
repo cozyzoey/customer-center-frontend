053d2c7aa7fb86c6506b7f885f82bda73ac151cd
cozyzoey <devzoeykim@gmail.com>
2022-07-25 22:52:14 +0900

    feat: 회원가입에 validation 추가

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,45 +1,46 @@
-import cookie from 'cookie'
-import { API_URL } from '@/static/config'
+import cookie from "cookie";
+import { API_URL } from "@/static/config";
 
 const register = async (req, res) => {
-  if (req.method === 'POST') {
-    const { username, email, password } = req.body
+  if (req.method === "POST") {
+    const { username, email, password } = req.body;
+    console.log(username, email, password);
 
     const strapiRes = await fetch(`${API_URL}/api/auth/local/register`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         username,
         email,
         password,
       }),
-    })
+    });
 
-    const { jwt, user, error } = await strapiRes.json()
+    const { jwt, user, error } = await strapiRes.json();
 
     if (strapiRes.ok) {
       // Set cookie, JWT를 클라이언트에 저장하지 않음
       res.setHeader(
-        'Set-Cookie',
-        cookie.serialize('token', jwt, {
+        "Set-Cookie",
+        cookie.serialize("token", jwt, {
           httpOnly: true,
-          secure: process.env.NODE_ENV !== 'development',
+          secure: process.env.NODE_ENV !== "development",
           maxAge: 60 * 60 * 24 * 7, // 1 week
-          sameSite: 'strict',
-          path: '/',
+          sameSite: "strict",
+          path: "/",
         })
-      )
+      );
 
-      res.status(200).json({ user })
+      res.status(200).json({ user });
     } else {
-      res.status(error.status).json({ message: error.message })
+      res.status(error.status).json({ message: error.message });
     }
   } else {
-    res.setHeader('Allow', ['POST'])
-    res.status(405).json({ message: `Method ${req.method} not allowed` })
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({ message: `Method ${req.method} not allowed` });
   }
-}
+};
 
-export default register
+export default register;
