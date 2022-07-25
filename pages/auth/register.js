f5b39425c31bff05222ca5f7717641c255f4f9d2
cozyzoey <devzoeykim@gmail.com>
2022-07-25 22:52:14 +0900

    feat: 회원가입에 validation 추가

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,61 +1,106 @@
-import { useContext, useState, useEffect } from 'react'
-import Link from 'next/link'
-import Layout from '@/components/Layout'
-import AuthContext from '@/context/AuthContext'
-import { toast } from 'react-toastify'
-import Button from '@/components/Button'
-import styles from '@/styles/shared/Auth.module.scss'
+import { useContext, useEffect } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import Layout from "@/components/Layout";
+import AuthContext from "@/context/AuthContext";
+import { toast } from "react-toastify";
+import Button from "@/components/Button";
+import styles from "@/styles/shared/Auth.module.scss";
 
 export default function register() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [username, setUsername] = useState('')
-
-  const { register, error } = useContext(AuthContext)
+  const { register, error } = useContext(AuthContext);
 
   useEffect(() => {
-    error && toast.error(error)
-  }, [error])
+    error && toast.error(error);
+  }, [error]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    register({ email, password, usernmae })
-  }
+  const initialValues = {
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    username: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email("이메일 형식을 확인해주세요")
+      .required("필수 입력 항목입니다"),
+    password: Yup.string()
+      .min(6, "6자 이상 입력해주세요")
+      .max(15, "15자 미만으로 입력해주세요")
+      .required("필수 입력 항목입니다"),
+    passwordConfirm: Yup.string()
+      .required("필수 입력 항목입니다")
+      .oneOf([Yup.ref("password"), null], "비밀번호가 다릅니다"),
+    username: Yup.string()
+      .min(3, "3자 이상 입력해주세요")
+      .max(15, "15자 미만으로 입력해주세요")
+      .required("필수 입력 항목입니다"),
+  });
+
+  const handleSubmit = (values) => {
+    const { email, password, username } = values;
+    register({ email, password, username });
+  };
+
+  const MyInput = ({ field, form, ...props }) => {
+    return (
+      <input
+        {...field}
+        {...props}
+        className={
+          form.touched?.[field.name] &&
+          form.errors?.[field.name] &&
+          styles.errorInput
+        }
+      />
+    );
+  };
 
   return (
     <Layout title="로그인/회원가입">
       <div className={styles.container}>
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <h1>회원가입하기</h1>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="이메일을 입력하세요"
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="비밀번호를 입력하세요"
-          />
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="이름을 입력하세요"
-          />
-          <Button type="submit" fullWidth={true} variant="outlined">
-            가입하기
-          </Button>
-          {/* <Link href="/auth/register">
-            <a>아직 회원이 아니신가요?</a>
-          </Link> */}
-          {/* <Link href="">
-            <a>비밀번호를 잊어버리셨나요?</a>
-          </Link> */}
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form className={styles.form} autoComplete="new-password">
+            <h1>회원가입하기</h1>
+            <Field
+              name="email"
+              type="email"
+              placeholder="이메일을 입력하세요"
+              component={MyInput}
+            />
+            <ErrorMessage component="label" name="email" />
+            <Field
+              name="password"
+              type="password"
+              placeholder="비밀번호를 입력하세요"
+              component={MyInput}
+            />
+            <ErrorMessage component="label" name="password" />
+            <Field
+              name="passwordConfirm"
+              type="password"
+              placeholder="비밀번호를 확인해주세요"
+              component={MyInput}
+            />
+            <ErrorMessage component="label" name="passwordConfirm" />
+            <Field
+              name="username"
+              type="text"
+              placeholder="이름을 입력하세요"
+              component={MyInput}
+            />
+            <ErrorMessage component="label" name="username" />
+            <Button type="submit" fullWidth={true} variant="outlined">
+              가입하기
+            </Button>
+          </Form>
+        </Formik>
       </div>
     </Layout>
-  )
+  );
 }
