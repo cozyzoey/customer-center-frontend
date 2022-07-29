c78c1cc2d59e3d7f4eb311446f33d04ec05b5b55
cozyzoey <devzoeykim@gmail.com>
2022-07-29 18:16:54 +0900

    update: 헤더 스타일

--- components/Header.js
+++ components/Header.js
@@ -1,40 +1,54 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import navLinks from "@/static/navLinks";
+import { motion, LayoutGroup } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { GrLogout } from "react-icons/gr";
+import { GrLogout, GrPrevious } from "react-icons/gr";
 
 import AuthContext from "@/context/AuthContext";
-import Button from "./button";
 import styles from "@/styles/header.module.scss";
 
 export default function Header({ logo }) {
   const router = useRouter();
   const { user, logout } = useContext(AuthContext);
+  const [pageTitle, setPageTitle] = useState("");
+
+  useEffect(() => {
+    setPageTitle(document.title);
+  }, []);
+
+  const isActiveNav = (navPath) => {
+    if (navPath === "/") {
+      return router.pathname === "/" || router.pathname.includes("notice");
+    } else {
+      return router.pathname === navPath || router.pathname.includes(navPath);
+    }
+  };
 
   return (
     <header className={styles.header}>
-      <ul className={styles.navLinks}>
-        {navLinks.map((nav, idx) => (
-          <li
-            key={idx}
-            className={
-              nav.path === "/"
-                ? router.pathname === "/" || router.pathname.includes("notice")
-                  ? styles.activeNavLink
-                  : styles.inactiveNavLink
-                : router.pathname === nav.path ||
-                  router.pathname.includes(nav.path)
-                ? styles.activeNavLink
-                : styles.inactiveNavLink
-            }
-          >
-            <Link href={nav.path}>
-              <a>{nav.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className={styles.prevBtn}>
+        <GrPrevious size="3ch" onClick={() => router.back()} />
+      </div>
+      <div className={styles.pageTitle}>{pageTitle}</div>
+      <LayoutGroup id="nav">
+        <motion.ul className={styles.navLinks}>
+          {navLinks.map((nav, idx) => (
+            <motion.li key={idx}>
+              <Link href={nav.path}>
+                <a>{nav.name}</a>
+              </Link>
+              {isActiveNav(nav.path) ? (
+                <motion.div
+                  layoutId="underline"
+                  className={styles.navUnderline}
+                  animate
+                ></motion.div>
+              ) : null}
+            </motion.li>
+          ))}
+        </motion.ul>
+      </LayoutGroup>
       <div className={styles.logo}>
         <Link href="/">
           <a>{logo} 고객센터</a>
@@ -45,17 +59,14 @@ export default function Header({ logo }) {
           {user ? (
             <>
               {user.username}
-              <GrLogout title="로그아웃" onClick={logout} size="2ch" />
+              <GrLogout title="로그아웃" onClick={logout} size="3ch" />
             </>
           ) : (
-            <Button onClick={() => router.push("/auth/login")} variant="text">
-              로그인/회원가입
-            </Button>
+            <Link href="/auth/login">
+              <a>로그인/회원가입</a>
+            </Link>
           )}
         </div>
-        <Button onClick={() => alert("hi")} variant="dark">
-          동의서 제출하기
-        </Button>
       </div>
     </header>
   );
