0141ec8ae7771b3d27d100ac06940e29d8fe3b72
cozyzoey <devzoeykim@gmail.com>
2022-07-30 13:30:09 +0900

    feat: 공지사항 페치 getStaticProps -> useSWR 변경

--- pages/notice/[id].js
+++ pages/notice/[id].js
@@ -1,46 +1,45 @@
 import qs from "qs";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 import moment from "moment";
 import parse from "html-react-parser";
 import Layout from "@/components/layout";
 import { API_URL } from "@/static/config";
+import { fetcher } from "@/helpers/index";
 import styles from "@/styles/shared/contents-detail.module.scss";
 
-export default function NoticeDetail({ notice }) {
-  return (
-    <Layout title="공지사항">
-      <h1 className={styles.title}>{notice.title}</h1>
-      <div className={styles.info}>
-        <time>{moment(notice.createdAt).format("YYYY. MM. DD")}</time>
-      </div>
-      <div className={styles.contents}>{parse(notice.contents)}</div>
-    </Layout>
-  );
-}
-
-export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/notices`);
-  const { data } = await res.json();
-
-  const paths = data.map((el) => ({ params: { id: String(el.id) } }));
-
-  return {
-    paths,
-    fallback: true,
-  };
-}
-
-export async function getStaticProps({ params: { id } }) {
+export default function NoticeDetail() {
+  const router = useRouter();
   const query = qs.stringify({
     filters: {
       id: {
-        $eq: id,
+        $eq: router.query?.id,
       },
     },
   });
-  const res = await fetch(`${API_URL}/api/notices?${query}`);
-  const { data } = await res.json();
-  return {
-    props: { notice: data[0].attributes },
-    revalidate: 10,
-  };
+  const {
+    data: { data },
+  } = useSWR(`${API_URL}/api/notices?${query}`, fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
+
+  return (
+    <Layout title="공지사항">
+      {data && (
+        <>
+          <h1 className={styles.title}>{data[0].attributes.title}</h1>
+          <div className={styles.info}>
+            <time>
+              {moment(data[0].attributes.createdAt).format("YYYY. MM. DD")}
+            </time>
+          </div>
+          <div className={styles.contents}>
+            {parse(data[0].attributes.contents)}
+          </div>
+        </>
+      )}
+    </Layout>
+  );
 }
