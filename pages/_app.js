0235f9bd6b26fa8a3e62fd398aabbbfedde48075
김반석 <70007794+cozyzoey@users.noreply.github.com>
2022-08-03 16:12:18 +0900

    feat: 데이터 페칭 전략 업데이트 (#1)
    * feat: loader 컴포넌트 추가
    
    * feat: 데이터 페치 전략 업데이트

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,30 @@
-import { ToastContainer } from 'react-toastify'
-import { AuthProvider } from '@/context/AuthContext'
-import '../styles/globals.scss'
-import 'react-toastify/dist/ReactToastify.css'
+import { SWRConfig } from "swr";
+import { fetcher } from "@/helpers/index";
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "@/context/AuthContext";
+import "../styles/globals.scss";
+import "react-toastify/dist/ReactToastify.css";
+
+import Loader from "@/components/loader";
+import { Suspense } from "react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Suspense fallback={<Loader />}>
+        <SWRConfig
+          value={{
+            fetcher: fetcher,
+            suspense: true,
+            revalidateIfStale: false,
+            revalidateOnFocus: false,
+          }}
+        >
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </SWRConfig>
+      </Suspense>
       <ToastContainer
         position="top-right"
         autoClose={3000}
@@ -21,7 +37,7 @@ function MyApp({ Component, pageProps }) {
         pauseOnHover
       />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
