3f78635c9733c7124cbf715fb1376f7ebffd0995
cozyzoey <devzoeykim@gmail.com>
2022-08-04 15:24:51 +0900

    feat: 공지사항에 NoDataHeading 추가

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import useFetchPage from "@/hooks/useFetchPage";
 import Layout from "@/components/layout";
 import Pagination from "@/components/pagination";
+import NoDataHeading from "@/components/no-data-heading";
 import tableStyles from "@/styles/shared/table.module.scss";
 
 export default function Home() {
@@ -10,31 +11,40 @@ export default function Home() {
 
   return (
     <Layout title="공지사항">
-      <div className={tableStyles.table}>
-        <dl className={tableStyles.tableHeader}>
-          <dd>번호</dd>
-          <dd>제목</dd>
-          <dd>작성일</dd>
-        </dl>
-        <ul>
-          {data.data.map((el) => (
-            <li key={el.id} className={tableStyles.tableRow}>
-              <Link href={`/notice/${el.id}`}>
-                <a>
-                  <dl key={el.id}>
-                    <dd>{el.id}</dd>
-                    <dd>{el.attributes.title}</dd>
-                    <dd>
-                      {moment(el.attributes.createdAt).format("YYYY. MM. DD")}
-                    </dd>
-                  </dl>
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Pagination page={page} total={data.meta.pagination.total} />
+      {data?.data.length === 0 && (
+        <NoDataHeading>아직 등록된 공지사항이 없습니다.</NoDataHeading>
+      )}
+      {data?.data.length > 0 && (
+        <>
+          <div className={tableStyles.table}>
+            <dl className={tableStyles.tableHeader}>
+              <dd>번호</dd>
+              <dd>제목</dd>
+              <dd>작성일</dd>
+            </dl>
+            <ul>
+              {data?.data.map((el) => (
+                <li key={el.id} className={tableStyles.tableRow}>
+                  <Link href={`/notice/${el.id}`}>
+                    <a>
+                      <dl key={el.id}>
+                        <dd>{el.id}</dd>
+                        <dd>{el.attributes.title}</dd>
+                        <dd>
+                          {moment(el.attributes.createdAt).format(
+                            "YYYY. MM. DD"
+                          )}
+                        </dd>
+                      </dl>
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <Pagination page={page} total={data.meta.pagination.total} />
+        </>
+      )}
     </Layout>
   );
 }
