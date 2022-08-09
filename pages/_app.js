61e51d418089b0d7eb026a068ef40bdbcbc8b2f3
cozyzoey <devzoeykim@gmail.com>
2022-08-09 10:39:43 +0900

    feat: favicon 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { SWRConfig } from "swr";
 import { fetcher } from "@/helpers/index";
 import { ToastContainer } from "react-toastify";
+import Head from "next/head";
 import { AuthProvider } from "@/context/AuthContext";
 import "../styles/globals.scss";
 import "react-toastify/dist/ReactToastify.css";
@@ -10,6 +11,29 @@ import { Suspense } from "react";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          ß
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Suspense fallback={<Loader />}>
         <SWRConfig
           value={{
