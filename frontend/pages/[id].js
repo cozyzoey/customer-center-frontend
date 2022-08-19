8c98ff8f8955e50329ca9a3be321fe6c43c1f09d
김반석 <70007794+cozyzoey@users.noreply.github.com>
2022-08-19 15:29:36 +0900

    Revert "feat: 상세내용 반응형"
    This reverts commit f67fe65431ce7e95eacf4d71e95fd739687d934d.

--- frontend/pages/[id].js
+++ frontend/pages/[id].js
@@ -1,46 +1,48 @@
 import qs from 'qs'
-import moment from 'moment'
-import parse from 'html-react-parser'
+import moment from 'moment';
+import parse from 'html-react-parser';
 import Layout from '@/components/Layout'
 import { API_URL } from '@/static/config'
-import styles from '@/styles/shared/ContentsDetail.module.scss'
+import styles from '@/styles/Notice.module.scss';
 
-export default function Notice({ notice }) {
+export default function Notice({notice}) {
   return (
-    <Layout title="공지사항">
-      <h1 className={styles.title}>{notice.title}</h1>
-      <div className={styles.info}>
+    <Layout title='공지사항'>
+      <div className={styles.notice}>
         <time>{moment(notice.createdAt).format('YYYY. MM. DD')}</time>
+        <h1>{notice.title}</h1>
+        <div className={styles.contents}>
+          {parse(notice.contents)}
+        </div>
       </div>
-      <div className={styles.contents}>{parse(notice.contents)}</div>
     </Layout>
   )
 }
 
 export async function getStaticPaths() {
   const res = await fetch(`${API_URL}/api/notices`)
-  const { data } = await res.json()
-
-  const paths = data.map((el) => ({ params: { id: String(el.id) } }))
+  const {data} = await res.json();
+  
+  const paths = data.map((el) => ({params: {id: String(el.id)}}))
 
   return {
     paths,
-    fallback: true,
+    fallback: true
   }
 }
 
-export async function getStaticProps({ params: { id } }) {
+export async function getStaticProps({params: {id}}) {
   const query = qs.stringify({
     filters: {
       id: {
-        $eq: id,
-      },
-    },
+        $eq: id
+      }
+    }
   })
   const res = await fetch(`${API_URL}/api/notices?${query}`)
-  const { data } = await res.json()
+  const {data} = await res.json()
   return {
-    props: { notice: data[0].attributes },
+    props: {notice: data[0].attributes},
     revalidate: 10,
   }
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,34 +6,29 @@ import Pagination from '@/components/Pagination'
 import { API_URL, PER_PAGE } from '@/static/config'
 
 import styles from '@/styles/Home.module.scss'
-import tableStyles from '@/styles/shared/Table.module.scss'
 
-export default function Home({ notices, page, total }) {
+export default function Home({notices, page, total}) {
   return (
-    <Layout title="공지사항">
-      <div className={tableStyles.table}>
-        <dl className={tableStyles.tableHeader}>
-          <dd>번호</dd>
-          <dd>제목</dd>
-          <dd>작성일</dd>
+    <Layout title='공지사항'>
+      <div className={styles.table}>
+        <dl className={styles.tableHeader}>
+        <dd>번호</dd>
+        <dd>제목</dd>
+        <dd>작성일</dd>
         </dl>
 
         <ul>
-          {notices.map((el) => (
-            <li key={el.id} className={tableStyles.tableRow}>
-              <Link href={`/${el.id}`}>
-                <a>
-                  <dl key={el.id}>
-                    <dd>{el.id}</dd>
-                    <dd>{el.attributes.title}</dd>
-                    <dd>
-                      {moment(el.attributes.createdAt).format('YYYY. MM. DD')}
-                    </dd>
-                  </dl>
-                </a>
-              </Link>
-            </li>
-          ))}
+          {notices.map((el) => <li>
+            <Link href={`/${el.id}`} key={el.id}>
+              <a>
+                <dl key={el.id} className={styles.tableRow}>
+                  <dd>{el.id}</dd>
+                  <dd>{el.attributes.title}</dd>
+                  <dd>{moment(el.attributes.createdAt).format('YYYY. MM. DD')}</dd>
+                </dl>
+              </a>
+            </Link>
+          </li>)}
         </ul>
       </div>
       <Pagination page={page} total={total} />
@@ -41,25 +36,24 @@ export default function Home({ notices, page, total }) {
   )
 }
 
-export async function getServerSideProps({ query: { page = 1 } }) {
+export async function getServerSideProps({query: {page = 1}}) {
   // 시작페이지 계산
-  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE
+  const start = +page === 1 ? 0 : (+page -1) * PER_PAGE;
 
-  const query = qs.stringify({
-    sort: 'createdAt:desc',
-    pagination: {
-      start: start,
-      limit: PER_PAGE,
-    },
-  })
+  const query = qs.stringify(
+    {sort: 'createdAt:desc',
+      pagination: {
+        start: start,
+        limit: PER_PAGE
+      }
+    }
+  )
   const res = await fetch(`${API_URL}/api/notices?${query}`)
-  const { data, meta } = await res.json()
+  const {data, meta} = await res.json();
 
   return {
     props: {
-      notices: data,
-      page: +page,
-      total: meta.pagination.total,
-    },
+      notices: data, page: +page, total: meta.pagination.total
+    }
   }
 }
