4ddfeb29abdf3e4b0b272ac558cf5d7cd83d42e2
김반석 <70007794+cozyzoey@users.noreply.github.com>
2022-08-19 15:29:36 +0900

    Revert "feat: ckeditor5 이미지 업로드 customAdapter 적용, 플러그인 수정 등"
    This reverts commit de96bca92f42d1bdbb40222ec0af38372c1a5bb2.

--- ckeditor5/src/ckeditor.js
+++ ckeditor5/src/ckeditor.js
@@ -6,22 +6,25 @@ import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
-import TableColumnResize from '@ckeditor/ckeditor5-table/src/tablecolumnresize.js';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
 
 class Editor extends ClassicEditor {}
 
@@ -30,22 +33,25 @@ Editor.builtinPlugins = [
 	Autoformat,
 	BlockQuote,
 	Bold,
+	CloudServices,
 	Essentials,
 	Heading,
 	Image,
-	ImageResize,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
 	ImageUpload,
 	Indent,
-	IndentBlock,
 	Italic,
 	Link,
 	List,
 	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
+	SimpleUploadAdapter,
 	Table,
-	TableColumnResize,
-	TableToolbar
+	TableToolbar,
+	TextTransformation
 ];
 
 // Editor configuration.
@@ -66,10 +72,20 @@ Editor.defaultConfig = {
 			'imageUpload',
 			'blockQuote',
 			'insertTable',
-			'mediaEmbed'
+			'mediaEmbed',
+			'undo',
+			'redo'
 		]
 	},
 	language: 'ko',
+	image: {
+		toolbar: [
+			'imageTextAlternative',
+			'imageStyle:inline',
+			'imageStyle:block',
+			'imageStyle:side'
+		]
+	},
 	table: {
 		contentToolbar: [
 			'tableColumn',
