bf403546c15b9df57ebd19f86a5d7a1dc2ad6353
김반석 <70007794+cozyzoey@users.noreply.github.com>
2022-08-19 15:29:36 +0900

    Revert "feat: AuthContext, 관련 api"
    This reverts commit e98b763606983212da24eae3644cb44f3da445e3.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,11 @@
 import { ToastContainer } from 'react-toastify'
-import { AuthProvider } from '@/context/AuthContext'
 import '../styles/globals.scss'
 import 'react-toastify/dist/ReactToastify.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Component {...pageProps} />
       <ToastContainer
         position="top-right"
         autoClose={3000}
