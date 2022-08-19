dd71d04fa3b1f7b00f0639c771900b756ed3bc9a
김반석 <70007794+cozyzoey@users.noreply.github.com>
2022-08-19 15:29:36 +0900

    Revert "fix: PER_PAGE 상수를 고정함 (env 사용시 문자열이라서 문제가 생김)"
    This reverts commit 5d6515cf1aa9b9b62bdae6700284c329a247e0ad.

--- static/config.js
+++ static/config.js
@@ -4,4 +4,4 @@ export const API_URL =
 export const NEXT_URL =
   process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
 
-export const PER_PAGE = 10;
+export const PER_PAGE = process.env.PER_PAGE || 3;
