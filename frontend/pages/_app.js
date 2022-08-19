eb770d9938f7b772c701fbdb820517e6070dd219
김반석 <70007794+cozyzoey@users.noreply.github.com>
2022-08-19 15:29:36 +0900

    Revert "feat: react-toastify 추가"
    This reverts commit 2e2836069ceb49855af55e73a75ead0d35c139bd.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,24 +1,7 @@
-import { ToastContainer } from 'react-toastify'
 import '../styles/globals.scss'
-import 'react-toastify/dist/ReactToastify.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
