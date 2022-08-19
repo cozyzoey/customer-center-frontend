aebca41116384222c79d63c30429f3ab79e07562
김반석 <70007794+cozyzoey@users.noreply.github.com>
2022-08-19 15:29:36 +0900

    Revert "feat: Header 반응형"
    This reverts commit 4a729c643fa02d44044798de193699495dc1d877.

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,58 +1,28 @@
-import { useContext } from 'react'
-import navLinks from '@/static/navLinks'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { GrLogout } from 'react-icons/gr'
+import navLinks from '@/static/navLinks';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-import AuthContext from '@/context/AuthContext'
-import Button from './Button'
-import styles from '@/styles/Header.module.scss'
+import Button from './Button';
+import styles from '@/styles/Header.module.scss';
+
 
 export default function Header() {
-  const router = useRouter()
-  const { user, logout } = useContext(AuthContext)
+  const router = useRouter();
 
   return (
     <header className={styles.header}>
       <ul className={styles.navLinks}>
-        {navLinks.map((nav, idx) => (
-          <li
-            key={idx}
-            className={
-              router.pathname === nav.path
-                ? styles.activeNavLink
-                : styles.inactiveNavLink
-            }
-          >
-            <Link href={nav.path}>
-              <a>{nav.name}</a>
+        {navLinks.map((nav, idx) => 
+          <li key={idx} className={router.pathname === nav.path ? styles.activeNavLink : styles.inactiveNavLink}>
+            <Link href={nav.path} >
+              <a >{nav.name}</a>
             </Link>
           </li>
-        ))}
+        )}
       </ul>
-      <div className={styles.logo}>
-        <Link href="/">
-          <a>고객센터</a>
-        </Link>
-      </div>
+      <div className={styles.logo}><Link href="/"><a>고객센터</a></Link></div>
       <div className={styles.myspace}>
-        <div className={styles.auth}>
-          {user ? (
-            <>
-              {user.username}
-              <div className="react-icons" title="로그아웃" onClick={logout}>
-                <GrLogout />
-              </div>
-            </>
-          ) : (
-            <Button onClick={() => router.push('/auth/login')} variant="text">
-              로그인/회원가입
-            </Button>
-          )}
-        </div>
-        <Button onClick={() => alert('hi')} variant="dark">
-          동의서 제출하기
-        </Button>
+        <Button onClick={() => alert("clicked")} variant='dark'>동의서 제출하기</Button>
       </div>
     </header>
   )
